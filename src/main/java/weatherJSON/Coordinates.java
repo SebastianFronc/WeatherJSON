8fd3ef6329ee2ccdba2596ed6d6ee694dd872d1d
Sebastian Fronc <fronc.sebastian@gmail.com>
2019-04-14 10:43:58 +0200

    Created WeatherCity class Created WeatherCityFactory class

---
+++ src/main/java/weatherJSON/Coordinates.java
@@ -0,0 +1,28 @@
+package weatherJSON;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Coordinates {
+
+
+
+    private final Double longitude;
+    private final Double latitude;
+
+    @JsonCreator
+    public Coordinates(
+        @JsonProperty("lon") Double longitude,
+        @JsonProperty("lat") Double latitude){
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+}
--- src/main/java/weatherJSON/WeatherCity.java
+++ src/main/java/weatherJSON/WeatherCity.java
@@ -1,6 +1,23 @@
 package weatherJSON;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class WeatherCity {
+    private final Integer id;
+    private final String name;
+    private final String country;
+    private final Coordinates  coordinates;
 
-
+    @JsonCreator
+    public WeatherCity(
+            @JsonProperty("id") Integer id,
+            @JsonProperty("name") String name,
+            @JsonProperty("country") String country,
+            @JsonProperty("coord") Coordinates coordinates) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.coordinates = coordinates;
+    }
 }
